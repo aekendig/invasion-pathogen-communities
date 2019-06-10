c35cfecfa6cfc298ae88d325f3cc83bd5de8aaf2
aekendig <aekendig@gmail.com>
2019-06-10 14:32:55 -0400

    fit iChao models

--- code/pathogen-community-analysis.R
+++ code/pathogen-community-analysis.R
@@ -15,7 +15,6 @@ rm(list = ls())
 library(tidyverse)
 library(SpadeR)
 library(brms)
-library(tidybayes)
 
 # import data
 d <- read.csv("./data/fungal-pathogens-2015-2017.csv")
@@ -76,28 +75,93 @@ dc %>%
   geom_point() + 
   facet_wrap(~year)
 
-# models
+# first full model
 mc1 <- brm(data = dc, family = gaussian,
            chao ~ native * year.s + (1|experiment) + (1|host),
            prior <- c(prior(normal(0, 100), class = Intercept),
                       prior(normal(0, 10), class = b),
                       prior(cauchy(0, 1), class = sd)),
-           iter = 5000, warmup = 1000, chains = 3, cores = 2,
-           control = list(adapt_delta = 0.9))
+           iter = 6000, warmup = 1000, chains = 1, cores = 1,
+           control = list(adapt_delta = 0.99))
 summary(mc1)
-marginal_effects(mc1)
-plot(mc1)
+pp_check(mc1, nsamples = 100) # values go below zero making the model distributions wider than the observed
+
+# second full model - log-transform richness
+dc$log_chao = log(dc$chao)
+mc1b <- update(mc1, formula. = log_chao ~ native * year.s + (1|experiment) + (1|host), newdata = dc)
+pp_check(mc1b, nsamples = 100) # closer than before
+summary(mc1b)
+
+# run full model with more chains
+mc1c <- update(mc1b, chains = 3, cores = 2,
+               control = list(adapt_delta = 0.9999, max_treedepth = 15))
+pp_check(mc1c, nsamples = 100)
+summary(mc1c)
+plot(mc1c) # convergence among chains
+plot(marginal_effects(mc1c), points = T) # richness is higher for native plants, increasing over time
 
 # subset models
-mc2 <- update(mc1, .~. -native:year.s)
-summary(mc2)
+mc2 <- update(mc1c, .~. -native:year.s)
 mc3 <- update(mc2, .~. -native)
-plot(mc3)
 mc4 <- update(mc2, .~. -year.s)
-plot(mc4)
 mc5 <- update(mc4, .~. -native)
 
 # model comparison
-lc = list(loo(mc1, reloo = T), loo(mc2, reloo = T), loo(mc3, reloo = T), loo(mc4, reloo = T), loo(mc5, reloo = T))
-lc
-loo_model_weights(lc)
+lc = list(loo(mc1c, reloo = T), loo(mc2, reloo = T), loo(mc3, reloo = T), loo(mc4, reloo = T), loo(mc5, reloo = T))
+lc # check that k values are low and that p_loo values are near number of parameters
+loo_model_weights(lc) # full model receives most of the weight followed by intercept-only model
+
+# save models
+save(mc1c, file = "./output/pathogen-community-analysis-chao-full.rda")
+save(mc2, file = "./output/pathogen-community-analysis-chao-main.rda")
+save(mc3, file = "./output/pathogen-community-analysis-chao-year.rda")
+save(mc4, file = "./output/pathogen-community-analysis-chao-origin.rda")
+save(mc5, file = "./output/pathogen-community-analysis-chao-intercept.rda")
+
+
+#### observed richness ####
+
+# dataset  - use same as for iChao
+
+# data-only figure
+dc %>%
+  mutate(richness = otus/isolates) %>%
+  ggplot(aes(x = grass.status, y = richness, shape = host)) +
+  geom_point() + 
+  facet_wrap(~year)
+
+# check mean and variance
+mean(dc$otus)
+var(dc$otus) # almost double
+
+# first full model
+mr1 <- brm(data = dc, family = negbinomial(),
+           otus ~ offset(isolates) + native * year.s + (1|experiment) + (1|host),
+           prior <- c(prior(normal(0, 100), class = Intercept),
+                      prior(normal(0, 10), class = b),
+                      prior(cauchy(0, 1), class = sd)),
+           iter = 6000, warmup = 1000, chains = 1, cores = 1,
+           control = list(adapt_delta = 0.999))
+summary(mr1)
+#### start here - predicted values very large, making this show up weird ####
+mr1_yrep <- posterior_predict(mr1)
+pp_check(mr1, nsamples = 100)
+# ppc_loo_pit_overlay
+
+# run full model with more chains
+mr1b <- update(mr1, chains = 3, cores = 2)
+pp_check(mr1b)
+summary(mr1b)
+plot(mc1c) # convergence among chains
+plot(marginal_effects(mc1c), points = T) # richness is higher for native plants, increasing over time
+
+# subset models
+mc2 <- update(mc1c, .~. -native:year.s)
+mc3 <- update(mc2, .~. -native)
+mc4 <- update(mc2, .~. -year.s)
+mc5 <- update(mc4, .~. -native)
+
+# model comparison
+lc = list(loo(mc1c, reloo = T), loo(mc2, reloo = T), loo(mc3, reloo = T), loo(mc4, reloo = T), loo(mc5, reloo = T))
+lc # check that k values are low and that p_loo values are near number of parameters
+loo_model_weights(lc) # full model receives most of the weight followed by intercept-only model
